cda5db2287d741d653f506c286e181bc0f8a75f2
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-03-09 18:16:59 +0000

    added code and files for website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+function calculateMath() {
+  document .getElementById("calculate-area").innerHTML = "<p>The area is: " + (5*3) + " cm²</p>"
+  document .getElementById("calculate-perimeter").innerHTML = "<p>The perimeter is: " + (5+3+5+3) + " cm</p>"
+}
